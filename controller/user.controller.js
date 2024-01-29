eeb7eb8f002ab612f4989f9f643f75f875473c16
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-30 01:06:06 +0300

    encrypt password and validate user login

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -13,4 +13,16 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createUser };
+// login 
+const loginUserCtrler = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    // check if user exists or not
+    const findUser = await User.findOne({ email });
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+        res.json(findUser);
+    } else {
+        throw new Error("Invalid Credentials");
+    }
+});
+
+module.exports = { createUser, loginUserCtrler };
--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose'); // Erase if already required
+const bcrypt = require('bcrypt');
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
@@ -25,6 +26,17 @@ var userSchema = new mongoose.Schema({
         required:true,
     },
 });
+
+// password encryption
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// match password for login
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
