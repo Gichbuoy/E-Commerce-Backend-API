ad4284c44957739dc371c0e833549d9085c6f728
Alex Maina <lexizgichbuoy@gmail.com>
2024-02-08 23:55:29 +0300

    product route && model

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -62,12 +62,32 @@ const handleRefreshToken = asyncHandler(async (req, res) => {
         }
         const accessToken = generateToken(user?._id);
         res.json({ accessToken });
-    });
-});
+    }); 
+});  
 
 
 // logout functionality
-const logout = asyncHandler(async (req, res) => {});
+const logout = asyncHandler(async (req, res) => {
+    const cookie = req.cookies;
+    if (!cookie?.refreshToken) throw new Error("No Refresh Token in Cookies");
+    const refreshToken = cookie.refreshToken;
+    const user = await User.findOne({ refreshToken });
+    if (!user) {
+        res.clearCookie("refreshToken", {
+            httpOnly: true,
+            secure: true,
+        });
+        return res.sendStatus(204); // forbidden
+    }
+    await User.findOneAndUpdate(refreshToken, {
+        refreshToken: "",
+    });
+    res.clearCookie("refreshToken", {
+        httpOnly: true,
+        secure: true,
+    });
+    res.sendStatus(204); // forbidden
+});
 
 
 // update a user
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
+const productRouter = require("./routes/productRoute");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middleware/errorHandler");
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/user", authRouter);
+app.use("/api/product", productRouter);
 
 app.use(notFound);
 app.use(errorHandler);
